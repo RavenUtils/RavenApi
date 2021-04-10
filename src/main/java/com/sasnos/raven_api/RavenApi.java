d00a2a2f0ca143006b946f0a6ec6d51095aa1922
Unbekannt <unbekannt-gitlab@protonmail.com>
2021-04-10 11:48:04 +0200

    make a dummy Main Mod class also remove unneeded stuff

--- src/main/java/com/sasnos/raven_api/RavenApi.java
+++ src/main/java/com/sasnos/raven_api/RavenApi.java
@@ -2,10 +2,6 @@ package com.sasnos.raven_api;
 
 import net.minecraftforge.fml.common.Mod;
 
-
-@Mod("rtaven_api")
-public class RavenApi
-{
-    public RavenApi() {
-    }
+@Mod("raven_api")
+public class RavenApi {
 }
